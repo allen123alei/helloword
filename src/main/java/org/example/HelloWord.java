13c9179705663e3e3bed56c064d6c02d9ca36b51
Administrator <allen1122alei@gmail.com>
2023-04-30 14:54:06 +0800

    初始项目提交

---
+++ src/main/java/org/example/HelloWord.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class HelloWord {
+
+    public static void main(String[] args) {
+        ArrayList<String> objects = new ArrayList<String>();
+
+        for (int i = 0; i <objects.size() ; i++) {
+            System.out.println("Hi dubai"+i);
+        }
+
+
+
+    }
+
+
+}
